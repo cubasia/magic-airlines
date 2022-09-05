014ef78597e04f035a3b839d218974fd570964bb
Giuseppe Fina <cubasia1622@gmail.com>
2022-09-05 12:50:09 +0200

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeContainerComponent } from './components/pages/home/home-container/home-container.component';
+import { HeaderComponent } from './components/pages/home/header/header/header.component';
+import { FooterComponent } from './components/pages/home/footer/footer/footer.component';
+import { MenuComponent } from './components/pages/home/menu/menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeContainerComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
