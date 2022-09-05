bc8b07b6b90a52188a6d1744ce73df8801e1431b
Giuseppe Fina <cubasia1622@gmail.com>
2022-09-05 16:14:14 +0200

    ok

--- src/app/components/pages/home/menu/home/menu.component.ts
+++ src/app/components/pages/home/menu/home/menu.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+interface prodotto {
+  image: string;
+  price: number;
+  description: string;
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
+  prodotti: prodotto[] = [
+    {
+      image: 'assets/foto/energy.jpg',
+      price: 39.99,
+      description: 'Energy Food',
+    },
+    { image: 'assets/foto/beans.jpg', price: 9.99, description: 'Beans' },
+    { image: 'assets/foto/peas.jpg', price: 6.99, description: 'Peas' },
 
-  constructor() { }
+    {
+      image: 'assets/foto/spaghetti.jpg',
+      price: 19.99,
+      description: 'Spaghetti',
+    },
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
